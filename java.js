41bae0a36deefe3ae53b256fc0f2a69bbbc757c5
I-Osim <osadh7@gmail.com>
2022-10-04 14:03:48 +0100

    trying javascript

---
+++ java.js
@@ -0,0 +1,9 @@
+// this is my first javascript lines today on 4th of October 2022
+let firstName = "Osim";
+let lastName = " Ibrahim";
+let middleName = " Adham";
+let greeting = " hello, Im";
+// this is the initial javascript codes i have written
+// lets write my name as js variables
+let fullName = firstName + lastName + middleName;
+// full name
--- javascript.js
+++
@@ -1,9 +0,0 @@
-// this is my first javascript lines today on 4th of October 2022
-let firstName = "Magnis";
-let lastName = " Quisque";
-let middleName = " Velit";
-let greeting = " hello, Im";
-console.log("this is the initial javascript codes i have written");
-console.log("lets write my name as js variables");
-let fullName = firstName + lastName + middleName;
-console.log("writing the full name", fullName);
